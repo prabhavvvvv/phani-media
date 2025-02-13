4cbaf07cdf61fd9d924d356dc78d1dbc1577ea68
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-02-13 14:32:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+x=0
+function Functionn(){
+    if (x==0){
+        document.getElementById("attribute1").style.visibility="visible";
+        document.getElementById("attribute1").className="border";
+        document.getElementById("attribute1").innerHTML="Social Media is very very powerful tool. It also gives power to tools.";
+        document.getElementById("attribute2").style.visibility="visible";
+        document.getElementById("attribute2").className="border";
+        document.getElementById("attribute2").innerHTML="The dark side of social media is that, within seconds, anything can be blown out of proportion and taken out of context.";
+        document.getElementById("anchovies").style.color="white";
+        document.getElementById("attribute3").style.visibility="visible";
+        document.getElementById("attribute3").className="border";
+        document.getElementById("attribute3").innerHTML="Social Media is not about the exploitation but service to the community.";
+        x=1;
+    }
+}
